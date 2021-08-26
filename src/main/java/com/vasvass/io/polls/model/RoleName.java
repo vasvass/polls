86697bd006aae3d8acad0edf52b673d5cb637b43
Vassilis Vassileiadis <vasvass@gmail.com>
2021-08-26 21:35:44 +0300

    Initial commit

---
+++ src/main/java/com/vasvass/io/polls/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.vasvass.io.polls.model;
+
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/vasvass/io/polls/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.vasvass.io.polls.repository;
+
+import com.vasvass.io.polls.model.RoleName;
+import com.vasvass.io.polls.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
